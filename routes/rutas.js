c0ffb45845de4d81e47499be9d95f222393c4743
401 <401@academicos.cesde.edu.co>
2022-04-19 11:31:44 -0500

    se realiza modificaciones en reserva de rutas

--- routes/rutas.js
+++ routes/rutas.js
@@ -12,6 +12,26 @@ rutas.post('/api/v1/habitaciones/', function (req, res) {
   rutas.get('/api/v1/habitaciones/', function (req, res) { 
     res.send('Hello World')
   })
-  //3.
+  //3. SERVICIO PARA BUSCAR POR ID
+  rutas.get('/api/v1/habitaciones/id/', function (req, res) { 
+    res.send('Hello World')
+  //4. SERVICIO PARA CAMBIAR HABITACIONES
+  rutas.put('/api/v1/habitaciones/', function (req, res) { 
+    res.send('Hello World')
+  //5.SERVICIO PARA ELIMINAR HABITACIONES
+  rutas.delete('/api/v1/habitaciones/', function (req, res) { 
+    res.send('Hello World')
+  //6.servicio para ingresar reserva
+  rutas.post('/api/v1/reserva/', function (req, res) { 
+    res.send('Hello World')
+  //7. servicio para buscar reserva por id
+  rutas.get('/api/v1/reserva/id/', function (req, res) { 
+    res.send('Hello World')
+  //8. servicio para editar reserva
+  rutas.put('/api/v1/reserva/', function (req, res) { 
+    res.send('Hello World')
+  //9. servicio para eliminar reserva
+  rutas.delete('/api/v1/reserva/', function (req, res) { 
+    res.send('Hello World')
 
 //RUTAS PARA LOS SERVICIOS DE RESERVA
